e2dac5987d8983befa8f9d2345ccac5e50f0981b
Charles Milette <charles.milette@gmail.com>
2017-09-07 11:53:46 -0400

    Basic settings implementation

--- Edge2Browser/Bing2Google.cs
+++ Edge2Browser/Bing2Google.cs
@@ -1,12 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using IniParser;
+using System;
+using System.IO;
 
 namespace SearchWithMyBrowser
 {
-    class Bing2Google
+    static class Bing2Google
     {
+        private enum SearchEngine
+        {
+            Google = 1,
+            DuckDuckGo = 2,
+            Bing = 3
+        }
+
+        private struct Settings
+        {
+            internal bool CustomEngineSelected;
+            internal string CustomEngineString;
+            internal SearchEngine Engine;
+        }
+
+        public static string GetCustomURL(string url)
+        {
+            return "lol";
+        }
+
+        private static Settings GetUsersSettings()
+        {
+            string file = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                "SearchWithMyBrowser",
+                "config.ini"
+            );
+
+            return LoadSettingsFile(file);
+        }
+
+        private static Settings LoadSettingsFile(string file)
+        {
+            var data = new FileIniDataParser().ReadFile(file)["SearchWithMyBrowser"];
+            return new Settings()
+            {
+                CustomEngineSelected = data["UseCustomEngine"].Equals("true"),
+                CustomEngineString = data["CustomEngineURL"],
+                Engine = (SearchEngine)int.Parse(data["Engine"])
+            };
+        }
     }
 }
