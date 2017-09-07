a3ae57ca62418e4774afbbd5b66a33c983087323
Charles Milette <charles.milette@gmail.com>
2017-09-07 10:32:14 -0400

    Initial implementation

--- Edge2Browser/Edge2Browser.cs
+++ Edge2Browser/Edge2Browser.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using System.Diagnostics;
 
 namespace SearchWithMyBrowser
 {
-    class Edge2Browser
+    static class Edge2Browser
     {
+        public static string ExtractURL(string url)
+        {
+            if (url.StartsWith("?launchContext1=", StringComparison.OrdinalIgnoreCase)) // Handle FCU
+            {
+                return ExtractFallCreatorsUpdateURL(url);
+            }
+
+            return url;
+        }
+
+        private static string ExtractFallCreatorsUpdateURL(string url)
+        {
+            var uri = new Uri(
+                HttpUtility.UrlDecode(
+                    HttpUtility.ParseQueryString(url)["url"]
+                )
+            );
+
+            return uri.AbsoluteUri + "?" + HttpUtility.UrlEncode(uri.Query);
+        }
     }
 }
--- Edge2Browser/Protocol2Url.cs
+++ Edge2Browser/Protocol2Url.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace SearchWithMyBrowser
 {
@@ -10,7 +7,35 @@ namespace SearchWithMyBrowser
     {
         static void Main(string[] args)
         {
-            // Main code here
+            if (args.Length != 0 && args[0].StartsWith("microsoft-edge:", StringComparison.OrdinalIgnoreCase))
+            {
+                string url = Edge2Browser.ExtractURL(args[0].Substring(15));
+
+                url = ValidateURL(url);
+
+                Process.Start(new ProcessStartInfo()
+                {
+                    FileName = url,
+                    UseShellExecute = true
+                });
+            }
         }
+
+        private static string ValidateURL(string url)
+        {
+            if (url.StartsWith("//"))
+                url = url.Substring(2);
+
+            if (!BeginsWithHTTP(url))
+                url = "http://" + url;
+
+            if (IsValidURL(url))
+                return url;
+            else
+                throw new UriFormatException();
+        }
+
+        private static bool IsValidURL(string url) => Uri.IsWellFormedUriString(url, UriKind.Absolute) && BeginsWithHTTP(url);
+        private static bool BeginsWithHTTP(string url) => url.StartsWith("http://", StringComparison.OrdinalIgnoreCase) || url.StartsWith("https://", StringComparison.OrdinalIgnoreCase);
     }
 }
