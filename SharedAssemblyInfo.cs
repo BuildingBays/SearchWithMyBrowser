b0bebba047b848e2e0847319385d6b7f0744f15a
Charles Milette <charles.milette@gmail.com>
2017-09-07 14:53:32 -0400

    Add actual sharedassemblyinfo file

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyCompany("Charles Milette")]
+[assembly: AssemblyProduct("SearchWithMyBrowser")]
+[assembly: AssemblyCopyright("Copyright © Charles Milette 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("en-US")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
