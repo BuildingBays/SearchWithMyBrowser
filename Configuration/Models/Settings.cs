aebc6f0d20303910852f85f9131752fbbf613e96
Charles Milette <charles.milette@gmail.com>
2017-09-08 10:34:32 -0400

    Basic UI work

---
+++ Configuration/Models/Settings.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MadMilkman.Ini;
+using Mvvm;
+
+namespace SearchWithMyBrowser.Models
+{
+    public enum SearchEngine
+    {
+        Custom = 0,
+        Google = 1,
+        DuckDuckGo = 2,
+        Bing = 3
+    }
+
+    public class Settings : BindableBase
+    {
+        private string _customURL;
+
+        [IniSerialization("CustomEngineURL")]
+        public string CustomURL
+        {
+            get => _customURL;
+            set => SetProperty<string>(ref _customURL, value, "CustomURL");
+        }
+
+        private SearchEngine _searchEngine;
+
+        [IniSerialization("Engine")]
+        public SearchEngine EngineSelection
+        {
+            get => _searchEngine;
+            set => SetProperty<SearchEngine>(ref _searchEngine, value, "EngineSelection");
+        }
+    }
+}
---
+++ Configuration/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,46 @@
+﻿using SearchWithMyBrowser.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mvvm;
+using MadMilkman.Ini;
+using System.IO;
+
+namespace SearchWithMyBrowser.ViewModels
+{
+    class MainWindowViewModel : BindableBase
+    {
+        Settings _settings;
+        public Settings CurrentSettings
+        {
+            get => _settings;
+            set => SetProperty<Settings>(ref _settings, value, "Settings");
+        }
+
+        public MainWindowViewModel()
+        {
+            _settings = LoadUserSettings();
+        }
+
+        private Settings LoadUserSettings()
+        {
+            string file = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                "SearchWithMyBrowser",
+                "config.ini"
+            );
+
+            return LoadSettingsFile(file);
+        }
+
+        private Settings LoadSettingsFile(string FileName)
+        {
+            IniFile file = new IniFile();
+            file.Load(FileName);
+            IniSection section = file.Sections.Add("SearchWithMyBrowser");
+            return section.Deserialize<Settings>();
+        }
+    }
+}
